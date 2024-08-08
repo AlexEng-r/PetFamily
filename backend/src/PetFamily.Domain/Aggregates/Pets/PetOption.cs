49e449e4d763b6be1e66116da57c8bdcbea62400
Myakishev A. <a.myakishev@teledoc.team>
2024-08-08 23:33:23 +0300

    удаление лишних сеттеров

--- backend/src/PetFamily.Domain/Aggregates/Pets/PetOption.cs
+++ backend/src/PetFamily.Domain/Aggregates/Pets/PetOption.cs
@@ -5,17 +5,17 @@ namespace PetFamily.Domain.Aggregates.Pets;
 public class PetOption
     : IValueObject
 {
-    public string? Description { get; private set; }
+    public string? Description { get; }
 
-    public string? Breed { get; private set; }
+    public string? Breed { get; }
 
-    public string? HealthInformation { get; private set; }
+    public string? HealthInformation { get; }
 
-    public double? Weight { get; private set; }
+    public double? Weight { get; }
 
-    public double? Height { get; private set; }
+    public double? Height { get; }
 
-    public DateTime? BirthDayDate { get; private set; }
+    public DateTime? BirthDayDate { get; }
 
     public PetOption(string? description, string? breed, string? healthInformation, double? weight, double? height,
         DateTime? birthDayDate)
