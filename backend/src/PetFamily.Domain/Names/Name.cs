cc252a2493c9159994a8352d7641abd8361dfe73
Myakishev A. <a.myakishev@teledoc.team>
2024-08-10 18:57:15 +0300

    Добавление волонтера, соц сетей, Имени

---
+++ backend/src/PetFamily.Domain/Names/Name.cs
@@ -0,0 +1,10 @@
+﻿namespace PetFamily.Domain.Fullname;
+
+public class Name
+{
+    public string FirstName { get; private set; }
+
+    public string Surname { get; private set; }
+
+    public string PatronymicName { get; private set; }
+}
---
+++ backend/src/PetFamily.Domain/Volunteers/Volunteer.cs
@@ -0,0 +1,29 @@
+﻿using PetFamily.Domain.Fullname;
+using PetFamily.Domain.Pets;
+using PetFamily.Domain.Requisites;
+using PetFamily.Domain.SocialNetworks;
+
+namespace PetFamily.Domain.Volunteers;
+
+public class Volunteer
+{
+    public Name FullName { get; private set; }
+
+    public string Description { get; private set; }
+
+    public int Experience { get; private set; }
+
+    public int PetsAdoptedCount { get; private set; }
+
+    public int PetsInSearchCount { get; private set; }
+
+    public int PetsOnTreatment { get; private set; }
+
+    public string Phone { get; private set; }
+
+    public List<SocialNetwork> SocialNetworks { get; private set; }
+
+    public List<Requisite> Requisites { get; private set; }
+
+    public List<Pet> Pets { get; private set; }
+}
