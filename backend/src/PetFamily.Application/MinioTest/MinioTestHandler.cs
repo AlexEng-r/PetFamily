203ee5ff35c0ed60d9230d1cfafa9ec321027dec
Myakishev A. <mr.alexchem@yandex.ru>
2024-09-04 11:56:20 +0300

    AddMinio

---
+++ backend/src/PetFamily.Application/MinioTest/MinioTestHandler.cs
@@ -0,0 +1,25 @@
+﻿using CSharpFunctionalExtensions;
+using PetFamily.Application.Providers;
+using PetFamily.Domain.Shared;
+
+namespace PetFamily.Application.MinioTest;
+
+public class MinioTestHandler
+{
+    private readonly IFileProvider _fileProvider;
+
+    public MinioTestHandler(IFileProvider fileProvider)
+    {
+        _fileProvider = fileProvider;
+    }
+
+    public Task<Result<string, Error>> GetFileAsync(string fileName, CancellationToken cancellationToken)
+        => _fileProvider.GetFileByNameAsync(fileName, cancellationToken);
+
+    public Task<Result<string, Error>> UploadFileAsync(Stream fileStream, string objectName,
+        CancellationToken cancellationToken)
+        => _fileProvider.UploadFileAsync(fileStream, objectName, cancellationToken);
+
+    public Task<Result<string, Error>> DeleteFileAsync(string fileName, CancellationToken cancellationToken)
+        => _fileProvider.DeleteFileAsync(fileName, cancellationToken);
+}
