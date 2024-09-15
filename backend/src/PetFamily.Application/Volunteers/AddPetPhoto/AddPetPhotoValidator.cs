422d1f8231fd667ae340b890434b85615946b595
Myakishev A. <mr.alexchem@yandex.ru>
2024-09-15 18:42:57 +0300

    фикс конфигурации

--- backend/src/PetFamily.Application/Volunteers/AddPetPhoto/AddPetPhotoValidator.cs
+++ backend/src/PetFamily.Application/Volunteers/AddPetPhoto/AddPetPhotoValidator.cs
@@ -1,5 +1,6 @@
 ﻿using FluentValidation;
 using PetFamily.Application.Validators;
+using PetFamily.Application.Volunteers.Common;
 using PetFamily.Domain.Shared;
 
 namespace PetFamily.Application.Volunteers.AddPetPhoto;
@@ -18,7 +19,16 @@ public class AddPetPhotoValidator
             .WithError(Errors.General.ValueIsRequired("PetId"));
 
         RuleForEach(p => p.Files)
-            .NotEmpty()
-            .WithError(Errors.General.ValueIsRequired("file"));
+            .SetValidator(new UploadFileDtoValidator());
+    }
+}
+
+public class UploadFileDtoValidator
+    : AbstractValidator<UploadFileDto>
+{
+    public UploadFileDtoValidator()
+    {
+        RuleFor(x => x.ObjectName).NotEmpty().WithError(Errors.General.ValueIsRequired());
+        RuleFor(x => x.Stream).Must(x => x.Length < 5000000);
     }
 }
